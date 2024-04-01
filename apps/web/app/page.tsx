c1129219481a1f59cc3e6bc1ccca09ba4beff59b
07anhad <anhadparashar07@gmail.com>
2024-04-01 16:53:35 +0530

    changed hexcode background

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1>Hi</h1>
+    <main className="w-full mx-auto max-w-5xl p-5">
+    <h1 className="font-bold text-4xl mt-10">Typing Mate</h1>
     </main>
   );
 }
