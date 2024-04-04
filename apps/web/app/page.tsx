dcfac1c403086883eac632ed65c11d52125f22bc
07anhad <anhadparashar07@gmail.com>
2024-04-04 11:36:20 +0530

    uuid for random invite odes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,38 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { toast } from "sonner";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  function createGame() {
+    console.log("Creating game...");
+    const inviteCode = uuidv4();
+    console.log("Generated invite code:", inviteCode);
+    router.push(`/game/${inviteCode}`);
+  }
+  
+
+  function joinGame(e:React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const inviteCode = formData.get('inviteCode') as string;
+
+    if (!inviteCode)
+      return toast.error('Invite Code is required');
+    
+    router.push(`/game/${inviteCode}`);
+  }
+
+
   return (
     <main className="w-full mx-auto max-w-5xl p-5">
       <h1 className="font-bold text-4xl mt-10">Typing Mate &nbsp;🚀</h1>
@@ -25,14 +54,14 @@ export default function Home() {
             </p>
           </div>
           <div>
-            <Button className="mt-5 w-full">
+            <Button className="mt-5 w-full" onClick={createGame}>
               Create Game As Host
             </Button>
           </div>
         </div>
         <div className="p-5 flex flex-col justify-between">
           <div>
-            <h2 className="font-medium text-2xl">Create Game</h2>
+            <h2 className="font-medium text-2xl">Join Game</h2>
             <p className="text-gray-400 mt-5">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
               dolor perferendis recusandae explicabo. Assumenda reiciendis
@@ -40,7 +69,7 @@ export default function Home() {
             </p>
           </div>
           <div className="mt-5">
-            <form >
+            <form onSubmit={joinGame}>
               <Input className="bg-custom-bg" type="text" placeholder="Invite Code" name="inviteCode"></Input>
               <Button className="mt-3 w-full">
               Join Game
